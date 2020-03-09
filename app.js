f4c46867558424e189e0688c8932d75d34937d84
Gaurang Torvekar <gaurangtorvekar@gmail.com>
2020-03-09 12:36:29 +0000

    Getting the time for the 3 locations

---
+++ app.js
@@ -0,0 +1,86 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+require('dotenv').config();
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
+app.get('/oauth', function(req, res) {
+    // When a user authorizes an app, a code query parameter is 
+    // passed on the oAuth endpoint. If that code is not there, 
+    // we respond with an error message
+    if (!req.query.code) {
+        res.status(500);
+        res.send({"Error": "Looks like we're not getting code."});
+        console.log("Looks like we're not getting code.");
+    } else {
+        // If it's there...
+
+        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+        request({
+            url: 'https://slack.com/api/oauth.access', //URL to hit
+            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+            method: 'GET', //Specify the method
+
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(body);
+
+            }
+        })
+    }
+});
+
+// Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
+app.post('/time', function(req, res) {
+    var indiaTime = new Date().toLocaleString("en-UK", {timeZone: "Asia/Kolkata"});
+	indiaTime = new Date(indiaTime);
+
+	var londonTime = new Date().toLocaleString("en-UK", {timeZone: "Europe/London"});
+	londonTime = new Date(londonTime);
+
+	var singaporeTime = new Date().toLocaleString("en-UK", {timeZone: "Asia/Singapore"});
+	singaporeTime = new Date(singaporeTime);
+    
+    console.log(indiaTime, londonTime, singaporeTime);
+    res.send("India = " + indiaTime.toLocaleString() + "\nLondon = " + londonTime.toLocaleString() + "\nSingapore = " + singaporeTime.toLocaleString());
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
